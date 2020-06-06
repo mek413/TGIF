383dfc3816c8ef9add1a8e59b555434d47061fc6
Marcus <mek413@gmail.com>
2020-06-05 22:52:50 -0500

    added form toggle for deliver path no functionality yet

--- tgif/src/compontents/Path/index.js
+++ tgif/src/compontents/Path/index.js
@@ -10,7 +10,7 @@ class Path extends React.Component {
         todoDiv : "col todo-selection hide",
         dinnerBtn1: "btn deliver-btn circle-btn",
         dinnerBtn2: "btn circle-btn",
-        todoBtn : "btn circle-btn hide"
+        delForm: "flex hide",
     };
 
     dinnerPath = (e) => {
@@ -18,15 +18,22 @@ class Path extends React.Component {
             this.setState({
                 dinner: "montBold dinner-select", dinnerDiv : "col dinner-selection",
                 todo: "montBold todo-select inactive", todoDiv : "col todo-selection hide",
-                dinnerBtn1: "btn deliver-btn circle-btn", dinnerBtn2: "btn circle-btn", todoBtn : "btn circle-btn hide"
+                dinnerBtn1: "btn deliver-btn circle-btn", dinnerBtn2: "btn circle-btn", todoBtn : "btn circle-btn hide",
+                delForm: "flex hide"
             });
     }
     todoPath = (e) => {
         e.preventDefault();
             this.setState({
                 dinner: "montBold dinner-select inactive", dinnerDiv : "col dinner-selection hide",
-                todo: "montBold todo-select", todoDiv : "col todo-selection",
-                dinnerBtn1: "btn circle-btn hide", dinnerBtn2: "btn circle-btn hide", todoBtn : "btn circle-btn"
+                todoDiv : "col todo-selection", todo: "montBold todo-select",
+                dinnerBtn1: "btn circle-btn hide", dinnerBtn2: "btn circle-btn hide"
+            });
+    }
+    startDelivery = (e) => {
+        e.preventDefault();
+            this.setState({dinnerBtn1: "btn circle-btn hide",
+                delForm: "flex"
             });
     }
 
@@ -36,27 +43,27 @@ class Path extends React.Component {
             <h2 className={this.state.dinner} onClick={this.dinnerPath}>1. WHAT'S FOR DINNER?</h2>
             <h2 className={this.state.todo} onClick={this.todoPath}>2. WHAT DO YOU WANT TO DO?</h2>
             <div className={this.state.dinnerDiv}>
-                <button id="deliverIt" className={this.state.dinnerBtn1}><span>Deliver it.</span></button>
-                {/* <form id="enterZip">
-                    <label id="locationInput" class="location" value="Zip Code" for="zipSearch">Enter Your Zip Code</label>
-                    <input text placeholder="55555" id="zipSearch"></input>
-                    <button class="zipVarStore btn">Search</button> 
-                </form>                        */}   
+                <button id="deliverIt" className={this.state.dinnerBtn1} onClick={this.startDelivery}><span>Deliver it.</span></button>
+                 <form id="enterZip" className={this.state.delForm}>
+                    <label id="locationInput" className="location" value="Zip Code" htmlFor="zipSearch">Enter Your Zip Code</label>
+                    <input type="text" placeholder="55555" id="zipSearch"></input>
+                    <button className="zipVarStore btn">Search</button> 
+                </form> 
             </div>
             <div className={this.state.dinnerDiv}>
                 <button id="getRecipe" className={this.state.dinnerBtn2}>Get Me a Recipe.</button>            
             </div>
 
             <div className={this.state.todoDiv}>
-                <button id="watchMovie" className={this.state.todoBtn}>Watch a Movie</button>
+                <button id="watchMovie" className="btn circle-btn">Watch a Movie</button>
             </div>
 
             <div className={this.state.todoDiv}>
-                <button id="listenMusic" className={this.state.todoBtn}>Listen to Music</button>
+                <button id="listenMusic" className="btn circle-btn">Listen to Music</button>
             </div>
 
             <div className={this.state.todoDiv}>
-                <button id="readBook" className={this.state.todoBtn}>Read a Book</button>
+                <button id="readBook" className="btn circle-btn">Read a Book</button>
             </div>
 
         </div>
